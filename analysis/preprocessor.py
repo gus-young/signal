5c3510c29b6f6219308986f804330b442627381a
Gus Young <76542590+gus-young@users.noreply.github.com>
2026-06-18 23:05:32 -0400

    ADDED - save_preprocessed function, which outputs numpy matricies to the output folder, requiring the preprocessing to only need to be run once, for efficiency

--- analysis/preprocessor.py
+++ analysis/preprocessor.py
@@ -1,5 +1,5 @@
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-import pandas as pd
+from analysis.loader import load_data, map_labels
 import numpy as np
 
 def get_feature_types(df):
@@ -30,3 +30,28 @@ def scale_features(train_df, test_df, numeric_features):
 def build_feature_matrix(scaled_numeric, encoded_categorical):
     X = np.hstack((scaled_numeric, encoded_categorical))
     return X
+
+def save_preprocessed():
+    df=load_data()
+    train_df = df[0]
+    test_df = df[1]
+
+    map_labels(train_df)
+    map_labels(test_df)
+    categorical_features = get_feature_types(train_df)[1]
+    numeric_features = get_feature_types(train_df)[0]
+
+    scaled_features = scale_features(train_df, test_df, numeric_features)
+    encoded_categoricals = encode_categoricals(train_df, test_df, categorical_features)
+
+    X_train = build_feature_matrix(scaled_features[1], encoded_categoricals[1])
+    y_train = train_df["attack_category"].values
+    
+    X_test = build_feature_matrix(scaled_features[2], encoded_categoricals[2])
+    y_test = test_df["attack_category"].values
+
+    np.save('output/X_train.npy', X_train)
+    np.save('output/y_train.npy', y_train)
+
+    np.save('output/X_test.npy', X_test)
+    np.save('output/y_test.npy', y_test)
