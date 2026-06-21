95cd09c1c2bc0f10fc1eb0887a0caef3ad0bc0ef
Gus Young <76542590+gus-young@users.noreply.github.com>
2026-06-21 00:24:51 -0400

    TEST - Generated plot with matplotlib

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-from analysis.loader import load_data
+from analysis.preprocessor import get_feature_types, encode_categoricals, scale_features, build_feature_matrix
+import numpy as np 
+import matplotlib.pyplot as plt
+from analysis.cluster import reduce_dimensions, run_kmeans, evaluate_clusters
 
-df=load_data()
-train_df = df[0]
-test_df = df[1]
-print(train_df.head())
+# Train Data
+X_train = np.load('output/X_train.npy')
+y_train = np.load('output/y_train.npy', allow_pickle=True)
+
+# Test Data
+X_test = np.load('output/X_test.npy')
+y_test = np.load('output/y_test.npy', allow_pickle=True)
+
+x_pca = reduce_dimensions(X_train, X_test)
+print(run_kmeans(x_pca[1]))
+
+x = []
+y = []
+
+for num in range (2,11):
+    kmeans_run = run_kmeans(x_pca[1], k=num)
+    clusters = evaluate_clusters (x_pca[1], kmeans_run)
+    x.append(num)
+    y.append(clusters[0])
+
+print (x)
+print (y)
+
+plt.plot(x, y)
+plt.xlabel = "k value"
+plt.ylabel = "Inertia Value"
+plt.title = "k vs Inertia"
+plt.savefig("plots/elbow.png")
