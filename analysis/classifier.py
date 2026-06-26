ae8d05dad6794e3d69a7c8b4542a78c16dd1e4ed
Gus Young <76542590+gus-young@users.noreply.github.com>
2026-06-25 23:12:43 -0400

    ADDED - Function that evaluates the model against the training data

--- analysis/classifier.py
+++ analysis/classifier.py
@@ -1,7 +1,11 @@
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.metrics import classification_report
 def train_classifier(X_train, y_train):
     rf_class = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1)
     fit_model = rf_class.fit(X_train, y_train)
     return fit_model
 
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, zero_division=0))
+    return y_pred
